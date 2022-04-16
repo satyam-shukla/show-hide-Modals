14e15ab7cb543c2d5c0db731c5c4246cac0a842f
satyam-shukla <satyam20@navgurukul.org>
2022-04-16 13:21:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const modal = document.querySelector(".modal");
+const overlay= document.querySelector(".overlay")
+const btnCloseModal=document.querySelector(".close-modal")
+const btnsOpenModal=document.querySelectorAll(".show-modal")
+// console.log(btnsOpenModal);
+
+const openModal=function(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+btnsOpenModal.forEach(element => 
+    element.addEventListener("click",openModal)
+    );
+    const closeModal=function(){
+        modal.classList.add("hidden")
+        overlay.classList.add("hidden")
+    }
+   btnCloseModal.addEventListener("click",closeModal);
+   overlay.addEventListener("click",closeModal);
+
+
+document.addEventListener("keydown",function(e){
+    if(e.key==='Escape' && !modal.classList.contains("hidden")){
+        closeModal();
+     }
+})
+
